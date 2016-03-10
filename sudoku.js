80d2f52065e8dda6e19dfc8cbe45058e4124e728
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2016-03-11 09:44:11 +1100

    began basic solve concept

--- sudoku.js
+++ sudoku.js
@@ -47,8 +47,51 @@ function applyInputToBoard() {
   $.each($('.row'), function(row, rowElement) {
     $.each($(rowElement).find('input'), function(col, input) {
       if (input.value) {
-        board[row][col] = parseInt(input.value, 10);
+        board[row][col] = [parseInt(input.value, 10)];
       }
     });
   });
 }
+
+// solve
+function solve() {
+  removeInvalidOptions();
+}
+
+function removeInvalidOptions() {
+  Object.keys(board).forEach(function(row) {
+    Object.keys(board[row]).forEach(function(col) {
+      if (board[row][col].length === 1) {
+        clearRowOfNumber(row, board[row][col][0]);
+        clearColOfNumber(col, board[row][col][0]);
+      }
+    });
+  });
+}
+
+function clearRowOfNumber(row, number) {
+  for (var col = 0; col < 9; col++) {
+    if (board[row][col].length > 1) {
+      board[row][col].remove(number);
+    }
+  }
+}
+
+function clearColOfNumber(col, number) {
+  for (var row = 0; row < 9; row++) {
+    if (board[row][col].length > 1) {
+      board[row][col].remove(number);
+    }
+  }
+}
+
+Array.prototype.remove = function(value) {
+  var index = this.indexOf(value);
+  if (index !== -1) {
+    this.splice(index, 1);
+  }
+  return this;
+};
+
+// TODO
+// decouple data structure
