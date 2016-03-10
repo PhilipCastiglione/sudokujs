00e792a8d5d38c590391faa3fecab894c798ca2a
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2016-03-11 09:12:37 +1100

    basic board establishment, render, setup inputs and apply inputs to board functions

--- sudoku.js
+++ sudoku.js
@@ -1,3 +1,54 @@
 // render
 // board object
 // solving functions
+//
+
+// establish board
+var board = {};
+for (var row = 0; row < 9; row++) {
+  board[row] = {};
+  for (var col = 0; col < 9; col++) {
+    board[row][col] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  }
+}
+
+// render
+function render() {
+  var $grid = $('<div>').attr('class', 'grid');
+  Object.keys(board).forEach(function(row) {
+    var $row = $('<div>').attr('class', 'row');
+    Object.keys(board[row]).forEach(function(col) {
+      var $col = $('<div>').attr('class', 'col cell')
+                           .html(board[row][col]);
+      $row.append($col);
+    });
+    $grid.append($row);
+  });
+  $('#grid-wrapper').html($grid);
+}
+
+// setup input
+function setupInput() {
+  var $grid = $('<div>').attr('class', 'grid');
+  Object.keys(board).forEach(function(row) {
+    var $row = $('<div>').attr('class', 'row');
+    Object.keys(board[row]).forEach(function(col) {
+      var $col = $('<div>').attr('class', 'col cell')
+                           .html($('<input>'));
+      $row.append($col);
+    });
+    $grid.append($row);
+  });
+  $('#grid-wrapper').html($grid);
+}
+
+// apply input to board
+function applyInputToBoard() {
+  $.each($('.row'), function(row, rowElement) {
+    $.each($(rowElement).find('input'), function(col, input) {
+      if (input.value) {
+        board[row][col] = parseInt(input.value, 10);
+      }
+    });
+  });
+}
